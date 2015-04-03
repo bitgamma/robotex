7a063e5723fa97dde8fb6546de83bd6fb2ddaaec
Michele Balistreri <michele@briksoftware.com>
2015-04-03 14:25:36 +0200

    read sensors

---
+++ lib/board/pirocon.ex
@@ -0,0 +1,63 @@
+defmodule Robotex.Board.Pirocon do
+  use GenServer
+
+  @motor_left_forward 10
+  @motor_left_backward 9
+
+  @motor_right_forward 8
+  @motor_right_backward 7
+
+  @obstacle_sensor_left 4
+  @obstacle_sensor_right 17
+
+  @line_sensor_left 18
+  @line_sensor_right 27
+
+  @sonar 14
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def release(pid) do
+    GenServer.call(pid, :release)
+  end
+
+  def read_obstacle_sensors(pid) do
+    GenServer.call(pid, :read_obstacle_sensors)
+  end
+
+  def read_line_sensors(pid) do
+    GenServer.call(pid, :read_line_sensors)
+  end
+
+  def init(_) do
+    {:ok, obstacle_sensor_left} = Gpio.start_link(@obstacle_sensor_left, :input)
+    {:ok, obstacle_sensor_right} = Gpio.start_link(@obstacle_sensor_right, :input)
+    {:ok, line_sensor_left} = Gpio.start_link(@line_sensor_left, :input)
+    {:ok, line_sensor_right} = Gpio.start_link(@line_sensor_right, :input)
+
+    %{obstacle_sensors: {obstacle_sensor_left, obstacle_sensor_right}, line_sensors: {line_sensor_left, line_sensor_right}}
+  end
+
+  def handle_call(:release, _from, state = %{obstacle_sensors: {obstacle_sensor_left, obstacle_sensor_right}, line_sensors: {line_sensor_left, line_sensor_right}}) do
+    Gpio.release(obstacle_sensor_left)
+    Gpio.release(obstacle_sensor_right)
+    Gpio.release(line_sensor_left)
+    Gpio.release(line_sensor_right)
+
+    {:reply, :ok, state}
+  end
+  def handle_call(:read_obstacle_sensors, _from, state = %{obstacle_sensors: {obstacle_sensor_left, obstacle_sensor_right}}) do
+    left = Gpio.read(obstacle_sensor_left)
+    right = Gpio.read(obstacle_sensor_right)
+
+    {:reply, {left, right}, state}
+  end
+  def handle_call(:read_line_sensors, _from, state = %{line_sensors: {line_sensor_left, line_sensor_right}}) do
+    left = Gpio.read(line_sensor_left)
+    right = Gpio.read(line_sensor_right)
+
+    {:reply, {left, right}, state}
+  end
+end
