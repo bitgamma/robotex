ffd938b292bd6602f7fe9b81f8a8818211397ade
Michele Balistreri <michele@briksoftware.com>
2015-04-06 13:22:32 +0200

    simplify cli

--- lib/robotex/cli.ex
+++ lib/robotex/cli.ex
@@ -1,28 +1,13 @@
 defmodule Robotex.CLI do
-  def main(argv) do
-    #TODO rewrite this almost completely
-    script_name = hd(argv)
-
+  def main([script_name | _]) do
     keyboard = Robotex.KeyboardInput.start([keys: ["q"]])
     [{module, _} | _] = Code.load_file(script_name)
-
-    main_pid = self
-
-    script_pid = spawn fn ->
-      cleanup_data = apply(module, :init, [])
-      send main_pid, {:script_cleanup_data, cleanup_data}
-      apply(module, :run, [cleanup_data])
-    end
-
-    script_cleanup_data = receive do
-      {:script_cleanup_data, cleanup_data} -> cleanup_data
-    end
+    script_pid = spawn fn -> apply(module, :run, []) end
 
     receive do
-      {:keyboard_event, _} -> Process.exit(script_pid, :kill)
+      {:keyboard_event, _} -> send(script_pid, :robotex_exit)
     end
 
-    apply(module, :cleanup, [script_cleanup_data])
     Process.exit(keyboard, :kill)
   end
 end
