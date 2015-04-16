c47f387a995a20c4953b08baf3c211bb32ba7a7a
Michele Balistreri <michele@briksoftware.com>
2015-04-16 10:44:40 +0200

    improved binary sensor array

--- lib/robotex/sensor/binary_array.ex
+++ lib/robotex/sensor/binary_array.ex
@@ -1,6 +1,11 @@
 defmodule Robotex.Sensor.BinaryArray do
   use GenServer
 
+  def start_link_with_pins(opts) do
+    {:ok, sensors} = Robotex.Sensor.Binary.start_link_multiple(opts)
+    Robotex.Sensor.BinaryArray.start_link(sensors: sensors)
+  end
+
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts)
   end
@@ -13,24 +18,23 @@ defmodule Robotex.Sensor.BinaryArray do
     GenServer.call(pid, :read)
   end
 
-  def set_notification(pid, target \\ self, trueOrFalse)
-  def set_notification(pid, target, true) do
-    GenServer.call(pid, {:set_notification, target})
+  def set_notification(pid, target \\ self, trueOrFalse, debounce_period \\ 5)
+  def set_notification(pid, target, true, debounce_period) do
+    GenServer.call(pid, {:set_notification, target, debounce_period})
   end
-  def set_notification(pid, _target, false) do
-    GenServer.call(pid, {:set_notification, nil})
+  def set_notification(pid, _target, false, _debounce_period) do
+    GenServer.call(pid, {:set_notification, nil, 0})
   end
 
   def init(opts) do
     sensor_list = Keyword.fetch!(opts, :sensors)
-    max_update_rate = Keyword.get(opts, :max_update_rate, 1)
 
     sensors = for sensor <- sensor_list do
       Robotex.Sensor.Binary.set_notification(sensor, true)
       {sensor, Robotex.Sensor.Binary.read(sensor)}
     end
 
-    {:ok, %{sensors: sensors, max_update_rate: max_update_rate, notified_pid: nil, send_timer: nil}}
+    {:ok, %{sensors: sensors, debounce_period: 0, notified_pid: nil, send_timer: nil}}
   end
 
   def handle_call(:stop, _from, state = %{sensors: sensors}) do
@@ -40,8 +44,8 @@ defmodule Robotex.Sensor.BinaryArray do
   def handle_call(:read, _from, state) do
     {:reply, read_values(state), state}
   end
-  def handle_call({:set_notification, notified_pid}, _from, state) do
-    {:reply, :ok, %{state | notified_pid: notified_pid}}
+  def handle_call({:set_notification, notified_pid, debounce_period}, _from, state) do
+    {:reply, :ok, %{state | notified_pid: notified_pid, debounce_period: debounce_period}}
   end
 
   def handle_info({:robotex_binary_sensor, sensor, _time, value}, state) do
@@ -62,9 +66,9 @@ defmodule Robotex.Sensor.BinaryArray do
   end
 
   defp notify_listener(state = %{notified_pid: nil}), do: state
-  defp notify_listener(state = %{notified_pid: notified_pid, max_update_rate: max_update_rate, send_timer: timer}) do
+  defp notify_listener(state = %{notified_pid: notified_pid, debounce_period: debounce_period, send_timer: timer}) do
     :timer.cancel(timer)
-    new_timer = :timer.send_after(max_update_rate, notified_pid, {:robotex_binary_sensor_array, read_values(state)})
+    new_timer = :timer.send_after(debounce_period, notified_pid, {:robotex_binary_sensor_array, read_values(state)})
 
     %{state | send_timer: new_timer}
   end
