1f81372ffde718596164e2719226e3cbcd4ce6d7
Michele Balistreri <michele@briksoftware.com>
2015-04-17 16:34:43 +0200

    fixed typo

--- lib/robotex/algorithm.ex
+++ lib/robotex/algorithm.ex
@@ -1,9 +1,9 @@
 defmodule Robotex.Algorithm do
   def start_link(algorithm, required_features) do
     parts = for feature <- required_features, into: %{} do
-      {feature, Process.whereis(required_features)}
+      {feature, Process.whereis(feature)}
     end
-    
+
     spawn_link(algorithm, :run, [parts])
   end
 end
