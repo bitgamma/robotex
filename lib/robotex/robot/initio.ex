4e1df45f56b22b48b54004f3279e2bba8e6b2846
Michele Balistreri <michele@briksoftware.com>
2015-04-17 10:29:29 +0200

    fixed typos

--- lib/robotex/robot/initio.ex
+++ lib/robotex/robot/initio.ex
@@ -19,8 +19,8 @@ defmodule Robotex.Robot.Initio do
   def __features__, do: [:sonar, :obstacle_sensors, :line_sensors, :locomotion, :pan_tilt]
 
   def init(:sonar), do: Robotex.Sensor.Sonar.start_link(pin: @sonar)
-  def init(:obstacle_sensors), do: Robotex.Sensor.BinaryArray.start_link_sensors(pins: @obstacle_sensor_left, @obstacle_sensor_right, logic_high: 0)
-  def init(:line_sensors), do: Robotex.Sensor.BinaryArray.start_link_sensors(pins: @line_sensor_left, @line_sensor_right)
+  def init(:obstacle_sensors), do: Robotex.Sensor.BinaryArray.start_link_sensors(pins: [@obstacle_sensor_left, @obstacle_sensor_right], logic_high: 0)
+  def init(:line_sensors), do: Robotex.Sensor.BinaryArray.start_link_sensors(pins: [@line_sensor_left, @line_sensor_right])
   def init(:locomotion), do: Robotex.Actuator.DCMotorPair.start_link_motors([forward_pin: @motor_left_forward, reverse_pin: @motor_left_reverse], [forward_pin: @motor_right_forward, reverse_pin: @motor_right_reverse])
   def init(:pan_tilt), do: Robotex.Actuator.PanTilt.start_link_servos([pin: @servo_pan], [pin: @servo_tilt])
 
@@ -29,5 +29,4 @@ defmodule Robotex.Robot.Initio do
   def stop(:line_sensors, sensors), do: Robotex.Sensor.BinaryArray.stop(sensors)
   def stop(:locomotion, motors), do: Robotex.Actuator.DCMotorPair.stop_motors(motors)
   def stop(:pan_tilt, servos), do: Robotex.Actuator.PanTilt.stop_servos(servos)
-
 end
