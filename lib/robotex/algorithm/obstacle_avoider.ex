1e4f5f98fb54e41e6a03dc07be85e860d8f8d0f6
Michele Balistreri <michele@briksoftware.com>
2015-05-01 14:38:52 +0200

    remove the sonar "animation" to improve energy usage

--- lib/robotex/algorithm/obstacle_avoider.ex
+++ lib/robotex/algorithm/obstacle_avoider.ex
@@ -2,10 +2,8 @@ defmodule Robotex.Algorithm.ObstacleAvoider do
   use GenServer
 
   @speed 100
-  @time_to_rotate_head 1000
-  @head_rotation_degrees 60
 
-  def __required_features__, do: [:sonar, :obstacle_sensors, :locomotion, :pan_tilt]
+  def __required_features__, do: [:obstacle_sensors, :locomotion]
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, [], opts)
@@ -16,7 +14,6 @@ defmodule Robotex.Algorithm.ObstacleAvoider do
   end
 
   def init(_) do
-    Robotex.Actuator.PanTilt.center(:pan_tilt)
     sensors_value = Robotex.Sensor.BinaryArray.read(:obstacle_sensors)
     react(sensors_value)
 
@@ -35,33 +32,12 @@ defmodule Robotex.Algorithm.ObstacleAvoider do
 
   defp react(sensors_value) do
     case sensors_value do
-      [true, true] ->
-        Robotex.Actuator.DCMotorPair.halt(:locomotion)
-        find_road
-      [true, false] ->
+      [true, _] ->
         Robotex.Actuator.DCMotorPair.spin_right(:locomotion, @speed)
       [false, true] ->
         Robotex.Actuator.DCMotorPair.spin_left(:locomotion, @speed)
       [false, false] ->
         Robotex.Actuator.DCMotorPair.forward(:locomotion, @speed)
     end
-  end
-
-  defp find_road do
-    left_distance = get_distance(-@head_rotation_degrees)
-    right_distance = get_distance(@head_rotation_degrees)
-    Robotex.Actuator.PanTilt.pan(:pan_tilt, 0)
-
-    if left_distance > right_distance do
-      Robotex.Actuator.DCMotorPair.spin_left(:locomotion, @speed)
-    else
-      Robotex.Actuator.DCMotorPair.spin_right(:locomotion, @speed)
-    end
-  end
-
-  defp get_distance(deg) do
-    Robotex.Actuator.PanTilt.pan(:pan_tilt, deg)
-    :timer.sleep(@time_to_rotate_head)
-    Robotex.Sensor.Sonar.read(:sonar)
   end
 end
